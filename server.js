d3dd95cb255fb3a42067cc5baa0e91c644a464e3
Steven Harradine <stevenharradine@gmail.com>
2017-02-07 09:37:12 -0500

    get new reviews compare against old and write new ones to existing records list

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ var google_play_id = process.argv[2]
 
 if (useStubbedResponse) {
 	fs.readFile(google_play_id + '.stub', 'utf8', function (err,data) {
-	  if (err) {
-	    return console.log(err)
-	  }
+	  if (err) return console.log(err)
 	  processDOM (data)
 	})
 } else {
@@ -29,28 +27,69 @@ if (useStubbedResponse) {
 
 	    res.on("end", function () {
 	        processDOM (content)
+
+	        // save stubbed response
+			fs.writeFile(google_play_id + '.stub', content, function(err) {
+		    	if(err) return console.log(err)
+			})
 	    });
 	}).on('error', function(e) {
 	  console.log("Got error: " + e.message)
 	});
-	req.end()	
+	req.end()
 }
 
 function processDOM (dom) {
 	$ = cheerio.load(dom)
 	var reviews = $('.single-review')
 
-
+	var new_json =  {}
+	    new_json.data = []
+	var existing_json = JSON.parse(fs.readFileSync(google_play_id + '.json', 'utf8'));
 
 	reviews.each (function (index, element) {
-		name = $(element).find(".author-name").text().trim()
-		date = $(element).find(".review-date").text().trim()
-		stars = $(element).find(".star-rating-non-editable-container").attr("aria-label").trim()
-		review = $(element).find(".review-body").text().trim()
+		var isFound = false;
 
-		console.log (name + "(" + date + ")")
-		console.log (stars)
-		console.log (review.replace("Full Review", ""))
-		console.log ()
+		name = cleanData ($(element).find(".author-name").text())
+		date = cleanData ($(element).find(".review-date").text())
+		stars = cleanData ($(element).find(".star-rating-non-editable-container").attr("aria-label"))
+		review = cleanData ($(element).find(".review-body").text())
+
+		// does the record exisit
+		for (var i = 0; i < existing_json.data.length; i++) {
+			if (existing_json.data[i].name == name && existing_json.data[i].date == date) {
+				isFound = true
+			}
+	 	}
+
+	 	// new record found
+		if (!isFound) {
+			current_record = {}
+
+			current_record.name = name
+			current_record.date = date
+			current_record.stars = stars
+			current_record.review = review
+
+			new_json.data.push (current_record)
+		}
+	})
+	
+	// add new records to the existing records
+	for (var i = 0; i < new_json.data.length; i++) {
+		console.log (existing_json.data.push (new_json.data[i]))
+	}
+
+	fs.writeFile(google_play_id + '.json', JSON.stringify(existing_json), function(err) {
+    	if(err) return console.log(err)
 	})
 }
+
+function cleanData (str) {
+	return str.trim().replaceAll("\"", "\\\"")
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
